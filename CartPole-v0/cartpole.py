1ec60d35caa9e4675c5f53121d376bc410301dc4
Loy van Beek <loy.vanbeek@gmail.com>
2017-02-04 14:41:22 +0100

    Get variables and reset when done

--- CartPole-v0/cartpole.py
+++ CartPole-v0/cartpole.py
@@ -5,4 +5,9 @@ env = gym.make('CartPole-v0')
 env.reset()
 for _ in range(1000):
     env.render()
-    env.step(env.action_space.sample()) # take a random action
+    
+    step = env.action_space.sample() # take a random action
+    obs, reward, done, info = env.step(step) 
+
+    if done:
+        env.reset()
